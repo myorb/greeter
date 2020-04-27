903be5d41b44d2c6d32b99418130e3391edc5976
alex <itdep@gmail.com>
2020-04-27 23:30:44 +0200

    add hello hendler

--- server.go
+++ server.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/myorb/greeter/greeter"
 )
 
 type server struct {
 	greeter.UnimplementedGreeterServer
+}
+
+func (*server) SayHello(ctx context.Context, req *greeter.HelloRequest) (*greeter.HelloReply, error) {
+	return &greeter.HelloReply{Message: fmt.Sprintf("hi %s", req.Name)}, nil
 }
